1e8da82cef012d6c397acfa274a28cdc05381cbe
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2022-02-10 00:23:34 +0900

    localStorage에 있는 거 새로고침해도 paint되게

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,9 @@
+var today = new Date();
+var year = today.getFullYear();
+var month = ('0' + (today.getMonth() + 1)).slice(-2);
+var day = ('0' + today.getDate()).slice(-2);
+var dateString = year + '-' + month  + '-' + day;
+
 const calendarProps={
   initialView: 'dayGridMonth',
   headerToolbar: {
@@ -5,16 +11,11 @@ const calendarProps={
     center: 'title',
     right: 'dayGridMonth,listMonth'
   },
-  initialDate: `2022-02-05`,
+  initialDate: dateString,
   navLinks: true, // can click day/week names to navigate views
   editable: true,
   dayMaxEvents: true, // allow "more" link when too many events
-  events: [
-    {
-      title: 'All Day Event',
-      start: '2021-02-05'
-    }
-  ],
+  events: [],
   dateClick: function(info) {
     alert('Clicked on: ' + info.dateStr);
     alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
@@ -26,4 +27,28 @@ var calendarEl = document.getElementById('calendar');
 var calendarId = new FullCalendar.Calendar(calendarEl, calendarProps );
 calendarId.render();
 
+function paintCalendar(){
+  const monthNames = ["January ", "February ", "March ", "April ", "May ", "June ",
+  "July ", "August ", "September ", "October ", "November ", "December "
+  ];
+  const titleDate = document.querySelector("#fc-dom-1").innerText;
+  const monthName = titleDate.replace(/[0-9]/g, "");
+  const yearString = titleDate.slice(-4);
+  const monthString = String(monthNames.indexOf(monthName)+1).padStart(2,'0');
+  const ID = `${yearString}-${monthString}`;
+  const savedSd = localStorage.getItem(ID);
+  if(savedSd!==null){
+    parsedSd = JSON.parse(savedSd);
+    parsedSd.forEach(element => calendarId.addEvent(element));
+    
+  }
+  
 
+}
+
+paintCalendar();
+
+let button = document.querySelectorAll(".fc-button-group button");
+for(i=0; i<2; i++){
+  button[i].addEventListener("click",paintCalendar);
+}
